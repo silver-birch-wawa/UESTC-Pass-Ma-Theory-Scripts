133815d22e7312d14ee588ee6793ea0bd4c24c90
N1CE_T0_MEET_Y0U <3193151055@qq.com>
2018-04-01 00:20:42 +0800

    Update fuck_uestc.py

--- fuck_uestc.py
+++ fuck_uestc.py
@@ -8,8 +8,14 @@ def click1(x,y):
 	win32api.SetCursorPos((x,y)) 
 	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0) 
 	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+def make_sure_secure():
+	for i in range(8):
+		win32api.keybd_event(13,0,0,0)     # 按下回车取消弹窗
+		win32api.keybd_event(13,0,win32con.KEYEVENTF_KEYUP,0)  #释放按键
+		time.sleep(1)
 for i in range(100):
 	try:
+		make_sure_secure();
 		click1(120,160)
 		time.sleep(5640)
 	except Exception as e:
